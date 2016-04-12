ccdbc3a024e742ff748ab8ace82fab86d39c49a3
Student32 <Student32@EEIT-PC>
2016-04-12 13:50:27 +0800

    Add new comment

--- proj-web/src/main/java/com/FH/proj_web/HelloServlet.java
+++ proj-web/src/main/java/com/FH/proj_web/HelloServlet.java
@@ -12,5 +12,6 @@ public class HelloServlet extends GenericServlet {
     @Override
     public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
         res.getWriter().println("Hello world!!");
+        res.getWriter().println("Happy New Year!!");
     }
 }
